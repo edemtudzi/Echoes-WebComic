bea88b6e1b5c930c16c4b81a723cd0252d7c4c57
edemtudzi <108690616+edemtudzi@users.noreply.github.com>
2026-07-02 21:46:46 +0000

    Import redundant panel cleanup styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import "./admin-progress-redesign.css";
 import "./mobile-priority.css";
 import "./asset-images.css";
 import "./reader-mobile-fix.css";
+import "./redundant-cleanup.css";
 import { getUser } from "@/lib/auth";
 import { signOut } from "@/app/actions/auth";
 import { GlobalFormLoader } from "@/components/GlobalFormLoader";
